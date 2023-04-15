b8b8ad841485d57c95fa63f53fa7af54815392e5
ahmedhafez11 <ahmedmohamedhafez1@yahoo.com>
2023-04-15 06:28:20 -0700

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * _stoi - converts string to integer
- * @s: string
- * Return: the int after converting it
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
-int _stoi(char *s)
+int _atoi(char *s)
 {
-	int i, j, n, len, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
-	j = 0;
+	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			++j;
+			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (j % 2)
+			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
@@ -34,28 +39,35 @@ int _stoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
+
 /**
- * main - multiplie two numbers
- * @arnum: number of arguments
- * @ararg: array of arguments
- * Return: 0 or 1
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
  */
-int main(int arnum, char *ararg[])
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (arnum < 3 || ararg > 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num1 = _stoi(ararg[1]);
-	num2 = _stoi(ararg[2]);
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
