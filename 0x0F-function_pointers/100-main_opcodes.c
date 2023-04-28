b475eeee940124d1404196b521b0080a3bc1c3e8
ahmedhafez11 <ahmedmohamedhafez1@yahoo.com>
2023-04-27 18:09:20 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the code
+ * @argc: number of elements 
+ * @argv: array
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int sz, i;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	sz = atoi(argv[1]);
+	if (sz < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < sz; i++)
+	{
+		if (i == sz - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
